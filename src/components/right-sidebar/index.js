da8b2f6e2b9bd8452eba78492364d3edab57a6f1
NisanurKorkmaz <nisanur29@live.fr>
2023-11-26 20:42:56 +0300

    add meetups content

--- src/components/right-sidebar/index.js
+++ src/components/right-sidebar/index.js
@@ -6,14 +6,17 @@ import { FaArrowRight, FaDribbble } from "react-icons/fa6";
 export const RightSidebar = () => {
     return (
         <div className="p-8">
+            {/* MEETUPS */}
             <div className="box-content w-42  h-100 sm:w-full mt-3 bg-[#262D34] flex flex-col  rounded-2xl ">
-                <div className="flex-row items-center  p-3 flex bg-[#d3dee8]">
+                {/* Header */}
+                <div className="flex-row items-center  p-3 flex">
                     Meetups
                     <div className=" pl-2 pt-1">
                         <FaArrowRight />
                     </div>
                 </div>
-                <div className="flex flex-col md:flex-row items-center bg-[#74818e]  w-full h-20 rounded-2xl p-5 mt-10 gap-6">
+                {/* Content */}
+                <div className="flex flex-col md:flex-row items-center w-full h-20 rounded-2xl p-5 gap-6">
                     <div className="flex item-center w-10 h-20 bg-[#2a3948] hover:bg-[#1b2024] rounded">
                         FEB
                         7
@@ -37,7 +40,7 @@ export const RightSidebar = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col md:flex-row items-center bg-[#74818e]  w-full h-20 rounded-2xl p-5 mt-10 gap-6">
+                <div className="flex flex-col md:flex-row items-center w-full h-20 rounded-2xl p-5 gap-6">
                     <div className="flex item-center w-10 h-20 bg-[#2a3948] hover:bg-[#1b2024] rounded">
                         FEB
                         7
@@ -60,7 +63,29 @@ export const RightSidebar = () => {
                         </div>
                     </div>
                 </div>
-
+                <div className="flex flex-col md:flex-row items-center w-full h-20 rounded-2xl p-5 gap-6">
+                    <div className="flex item-center w-10 h-20 bg-[#2a3948] hover:bg-[#1b2024] rounded">
+                        FEB
+                        7
+                    </div>
+                    <div className="flex-col">
+                        <div className="items-center text-[12px] text-[#e3e5e9] no-underline hover:underline cursor-pointer pb-1"> Design Meetups USA | Dribbble</div>
+                        <div className="flex">
+                            <div className=" bg-[#a22b5f] w-4 h-4 rounded-full">
+                                <FaDribbble />
+                            </div>
+                            <div className="items-center text-[10px] text-[#C5D0E6] pl-2">Dribbble • Austin, Texas, USA</div>
+                        </div>
+                        <div className="flex items-center justify-center md:justify-start gap-2 mt-3">
+                            <button className="flex items-center justify-center bg-[#2C353D] rounded-2xl px-3 py-1 hover:opacity-80">
+                                <span className="text-[8px] text-[#C5D0E6] font-semibold">Remote</span>
+                            </button>
+                            <button className="flex items-center justify-center bg-[#2C353D] rounded-2xl px-3 py-1 hover:opacity-80">
+                                <span className="text-[8px] text-[#C5D0E6] font-semibold">Part Time</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className="box-content h-60  w-screen sm:w-full mt-3 bg-[#262D34] flex flex-col rounded-2xl ">
